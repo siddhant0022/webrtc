c56c185c585a18b1f4a7cf5954a0d598e49d5382
siddhant0022 <148846164+siddhant0022@users.noreply.github.com>
2025-06-24 23:50:24 +0530

    first commit

--- twil.js
+++ twil.js
@@ -1,9 +1,7 @@
 const twilio = require('twilio');
 
 // Replace with your actual credentials from Twilio Console
-const accountSid = 'AC9ab131cef39135bc4be8432f7bd46b4f';
-const authToken = 'f59595621008c9cfd0e977159e359ee0';
-const client = twilio(accountSid, authToken);
+
 
 // Generate TURN credentials
 async function getTurnCredentials() {
